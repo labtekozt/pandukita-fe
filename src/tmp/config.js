33c1c1190cdcd2b54bbd9103c759f3b677579cec
EgiStr <egicuco50@gmail.com>
2023-07-12 01:49:24 +0700

    version 1.0.0

--- src/tmp/config.js
+++ src/tmp/config.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://47.89.245.22/api/v1";
-export const Domain = "http://47.89.245.22";
+export const baseURL = "https://chatdosen.my.id/api/v1";
+export const Domain = "https://chatdosen.my.id";
