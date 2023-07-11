162af66f9dfebcdda3524cd93d4e60890c5e9f41
EgiStr <egicuco50@gmail.com>
2023-07-12 04:13:39 +0700

    version 1.0.0 PWA

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,25 @@ import "react-datepicker/dist/react-datepicker.css";
 import { GlobalProvider } from "./store/index.jsx";
 import Toast from "./component/Toast.jsx";
 import { PlanProvaider } from "./store/plan/index.jsx";
+import { registerSW } from "virtual:pwa-register";
+if ("serviceWorker" in navigator) {
+  const updateSW = registerSW({
+    onNeedRefresh() {
+      const isUpdate = window.confirm(
+        "New Update Available. Click OK to update"
+      );
+      if (isUpdate) {
+        updateSW(true);
+      }
+    },
+    onOfflineReady() {
+      console.log("offline ready");
+    },
+  });
+}
 
 import L from "leaflet";
-L.Icon.Default.imagePath = "/leaflet_images/"; 
+L.Icon.Default.imagePath = "/leaflet_images/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
