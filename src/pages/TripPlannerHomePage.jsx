37d48e22eb996d97e36faa737e4609807e513f27
Eggi Satria <66462608+EgiStr@users.noreply.github.com>
2023-07-21 17:31:14 +0700

    fix plannerPage

--- src/pages/TripPlannerHomePage.jsx
+++ src/pages/TripPlannerHomePage.jsx
@@ -77,7 +77,7 @@ function TripPlannerHome() {
                 data.map((item, i) => {
                   if (data.length === i + 1) {
                     return (
-                      <div className="mt-[175px] mb-[100px]" key={i} ref={lastOrder}>
+                      <div className="mt-[175px] mb-[150px]" key={i} ref={lastOrder}>
                         <div className="mb-[-55px]">
                           <Link to={`/planner/${item._id}`}>
                             <div className="trip-text mb-[-210px]">
@@ -114,7 +114,7 @@ function TripPlannerHome() {
                     );
                   } else {
                     return (
-                      <div className="mt-[175px]" key={i}>
+                      <div className="mt-[175px] mb-[100px]" key={i}>
                         <div className="mb-[-55px]">
                           <Link to={`/planner/${item._id}`}>
                             <div className="trip-text mb-[-210px]">
