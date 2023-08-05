c32242ad60c7bb48d02565908e978ed967d204c2
EgiStr <egicuco50@gmail.com>
2023-08-06 06:50:39 +0700

    build:version 1.0.0

--- src/tmp/config.js
+++ src/tmp/config.js
@@ -1,6 +1,6 @@
-export const baseURL = "http://127.0.0.1:8000/api/v1";
-export const Domain = "http://127.0.0.1:8000";
+// export const baseURL = "http://127.0.0.1:8000/api/v1";
+// export const Domain = "http://127.0.0.1:8000";
 export const AlamatUrl = `https://alamat.thecloudalert.com/api/cari/index/?keyword=${"keyword"}`;
 
-// export const baseURL = "https://chatdosen.my.id/api/v1";
-// export const Domain = "https://chatdosen.my.id";
+export const baseURL = "https://chatdosen.my.id/api/v1";
+export const Domain = "https://chatdosen.my.id";
