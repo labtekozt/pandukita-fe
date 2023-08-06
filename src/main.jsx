0904ad359868ca28f851451c1bc0275877904162
EgiStr <egicuco50@gmail.com>
2023-08-06 07:24:17 +0700

    build:version !.0.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { router } from "./App.jsx";
-import "./index.css";
-import "leaflet/dist/leaflet.css";
-import { RouterProvider } from "react-router-dom";
-import "./disk/css/main.css";
-import "./App.css";
-import "react-datepicker/dist/react-datepicker.css";
 import { GlobalProvider } from "./store/index.jsx";
 import Toast from "./component/popUp/Toast.jsx";
 import Notification from "./component/popUp/Notification.jsx";
 import { PlanProvaider } from "./store/plan/index.jsx";
 import { registerSW } from "virtual:pwa-register";
+import { RouterProvider } from "react-router-dom";
+import ModalConfirm from "./component/Modal/Comfirm.jsx";
+import ModalWaitOrder from "./component/Modal/WaitOrder.jsx";
+
+import "./index.css";
+import "leaflet/dist/leaflet.css";
+import "./disk/css/main.css";
+import "./App.css";
+import "react-datepicker/dist/react-datepicker.css";
 if ("serviceWorker" in navigator) {
   const updateSW = registerSW({
     onNeedRefresh() {
@@ -29,8 +32,6 @@ if ("serviceWorker" in navigator) {
 }
 
 import L from "leaflet";
-import ModalConfirm from "./component/Modal/comfirm.jsx";
-import ModalWaitOrder from "./component/Modal/WaitOrder.jsx";
 L.Icon.Default.imagePath = "/leaflet_images/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
