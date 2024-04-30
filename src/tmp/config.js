138b76055ae8d7bcdfd5f24e128f7c59c4beb47d
EgiStr <egicuco50@gmail.com>
2024-05-01 00:03:31 +0700

    fix: change API url

--- src/tmp/config.js
+++ src/tmp/config.js
@@ -2,5 +2,5 @@
 // export const Domain = "http://127.0.0.1:8000";
 export const AlamatUrl = `https://alamat.thecloudalert.com/api/cari/index/?keyword=${"keyword"}`;
 
-export const baseURL = "https://chatdosen.my.id/api/v1";
-export const Domain = "https://chatdosen.my.id";
+export const baseURL = "http://103.226.139.96/api/v1";
+export const Domain = "http://103.226.139.96";
